6c874460bb5781c36626f5beec1481f26766bda3
Alex DeMeo <alex.demeo129@gmail.com>
2021-11-18 09:50:11 -0800

    the old way works tho

--- proxy.js
+++ proxy.js
@@ -11,17 +11,13 @@ const HOST = '0.0.0.0';
 app.use(cors());
 app.use(morgan('dev'));
 
-app.use((req, res, next) => {
-  if (/(.ico|.js|.css|.jpg|.png|.map)$/i.test(req.path)) {
-    next();
-  } else {
-    res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
-    res.header('Expires', '-1');
-    res.header('Pragma', 'no-cache');
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-  }
+// app.use(express.static(path.join(__dirname, 'build')));
+app.use('/static', express.static(path.join(__dirname, 'build//static')));
+
+app.get('/*', (req, res) => {
+  console.log('response static to', req.hostname, 'ip', req.ip, 'path', req.path);
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-app.use(express.static(path.join(__dirname, 'build')));
 
 app.use('/roku', proxy('192.168.1.226:8060'));
 app.use('/coffee', proxy('pi3.local:5000'));
