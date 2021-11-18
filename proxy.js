cb03e1e76172625a22732b1852f5e9e6f4f5a7ff
Alex DeMeo <alex.demeo129@gmail.com>
2021-11-18 12:47:31 -0800

    try adding ssl

--- proxy.js
+++ proxy.js
@@ -3,15 +3,16 @@ const cors = require('cors');
 const morgan = require('morgan');
 const path = require('path');
 const proxy = require('express-http-proxy');
+const https = require('https');
+const fs = require('fs');
 const app = express();
 
-const PORT = process.env.PORT || 4000;
+const PORT = +(process.env.PORT || 443);
 const HOST = '0.0.0.0';
 
 app.use(cors());
 app.use(morgan('dev'));
 
-// app.use(express.static(path.join(__dirname, 'build')));
 app.use('/static', express.static(path.join(__dirname, 'build//static')));
 
 app.get('/*', (req, res) => {
@@ -23,6 +24,17 @@ app.use('/roku', proxy('192.168.1.226:8060'));
 app.use('/coffee', proxy('pi3.local:5000'));
 app.use('/printer', proxy('pi2.local:5555'));
 
-app.listen(PORT, HOST, () => {
-  console.log(`Starting Proxy at ${HOST}:${PORT}`);
-});
+https
+  .createServer(
+    {
+      key: fs.readFileSync(path.join(__dirname, '.ssl', 'key.pem'), 'utf8'),
+      cert: fs.readFileSync(path.join(__dirname, '.ssl', 'cert.pem'), 'utf8'),
+      passphrase: process.env.PASSPHRASE,
+      requestCert: false,
+      rejectUnauthorized: false,
+    },
+    app,
+  )
+  .listen(PORT, HOST, function () {
+    console.log('Express server listening on port ' + PORT);
+  });
