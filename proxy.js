a241a164e77f422ae7e3f0b1b5e3a8b938f4ca10
Alex DeMeo <alex.demeo129@gmail.com>
2021-11-17 21:39:38 -0800

    i think prod version works now

--- proxy.js
+++ proxy.js
@@ -6,10 +6,17 @@ const proxy = require('express-http-proxy');
 const app = express();
 
 const PORT = process.env.PORT || 4001;
-const HOST = 'localhost';
+const HOST = '0.0.0.0';
 
 app.use(cors());
 app.use(morgan('dev'));
+
+app.use('/*', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 app.use('/roku', proxy('192.168.1.226:8060'));
 app.use('/coffee', proxy('pi3.local:5000'));
@@ -18,11 +25,10 @@ app.use('/printer', proxy('pi2.local:5555'));
 
 app.use(express.static(path.join(__dirname, 'build')))
 
-app.get('/', (req, res) => {
+app.get('/*', (req, res) => {
+  console.log('response static to', req.hostname, 'ip', req.ip, 'path', req.path)
   res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
-
-app.listen(8080)
 
 app.listen(PORT, HOST, () => {
   console.log(`Starting Proxy at ${HOST}:${PORT}`);
--- src/utils/network.ts
+++ src/utils/network.ts
@@ -1,7 +1,7 @@
 import { ActionRequest, ActionResponse } from '../static/types';
 
 export async function network(request: ActionRequest): Promise<ActionResponse> {
-  const url = `${process.env.NODE_ENV === 'production' ? '127.0.0.1:4001' : ''}/${request.remote}${request.endpoint}`;
+  const url = `${process.env.NODE_ENV === 'production' ? '' : ''}/${request.remote}${request.endpoint}`;
   console.log('fetch(', url, ')');
   const fetchResult = await fetch(url, {
     method: 'POST',
