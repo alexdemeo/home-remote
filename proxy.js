4d1000cd8e7147a36fd968ee9d8afa59dc4ed0e7
Alex DeMeo <alex.demeo129@gmail.com>
2021-11-18 15:15:01 -0800

    fuck ssl

--- proxy.js
+++ proxy.js
@@ -3,11 +3,9 @@ const cors = require('cors');
 const morgan = require('morgan');
 const path = require('path');
 const proxy = require('express-http-proxy');
-const https = require('https');
-const fs = require('fs');
 const app = express();
 
-const PORT = +(process.env.PORT || 443);
+const PORT = +(process.env.PORT || 80);
 const HOST = '0.0.0.0';
 
 app.use(cors());
@@ -24,17 +22,6 @@ app.use('/roku', proxy('192.168.1.226:8060'));
 app.use('/coffee', proxy('pi3.local:5000'));
 app.use('/printer', proxy('pi2.local:5555'));
 
-https
-  .createServer(
-    {
-      key: fs.readFileSync(path.join(__dirname, '.ssl', 'key.pem'), 'utf8'),
-      cert: fs.readFileSync(path.join(__dirname, '.ssl', 'cert.pem'), 'utf8'),
-      passphrase: process.env.PASSPHRASE,
-      requestCert: false,
-      rejectUnauthorized: false,
-    },
-    app,
-  )
-  .listen(PORT, HOST, function () {
-    console.log('Express server listening on port ' + PORT);
-  });
+app.listen(PORT, HOST, function () {
+  console.log('Express server listening on port ' + PORT);
+});
