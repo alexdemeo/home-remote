b713a667a56e731bac6882df3b1e30fa60c5ef6a
Alex DeMeo <alex.demeo129@gmail.com>
2022-01-31 18:28:54 -0800

    add NODE_ENV == prod flag

--- public/electron.js
+++ public/electron.js
@@ -4,14 +4,18 @@ const BrowserWindow = electron.BrowserWindow;
 
 let mainWindow;
 
+const PROD_URL = 'http://pi4.local';
+const DEV_URL = 'http://localhost:4001';
+
 function createWindow() {
   mainWindow = new BrowserWindow({
     width: 560,
     height: 1080,
     frame: false,
   });
-  mainWindow.loadURL(`http://pi4.local`);
-  // mainWindow.loadURL(`http://localhost:4001`);
+  const url = process.env.HOME_ENV === 'development' ? DEV_URL : PROD_URL;
+  console.log('Loading from:', url);
+  mainWindow.loadURL(process.env.HOME_ENV === 'development' ? DEV_URL : PROD_URL);
   mainWindow.on('closed', () => (mainWindow = null));
 }
 
